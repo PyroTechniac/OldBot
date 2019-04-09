c8fd970e2f924829b7dc8dbcedcddc5db5dfcf4c
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-08 21:49:42 -0400

    fixed port

--- commands/Testing/route.js
+++ commands/Testing/route.js
@@ -18,7 +18,7 @@ module.exports = class extends Command {
     }
 
     async run(message) {
-        const test = await fetch(`https://botpyro-testing.herokuapp.com:1234/api/guilds/${message.guild.id}`)
+        const test = await fetch(`https://botpyro-testing.herokuapp.com/api/guilds/${message.guild.id}`)
             .then(res => res.json());
 
         this.client.console.log(test);
