6a1bd15ea0944d3b4c819e519597a1dd6fd48a2e
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-10 18:33:32 -0400

    findUser route

---
+++ routes/findUser.js
@@ -0,0 +1,14 @@
+const { Route } = require('klasa-dashboard-hooks');
+
+module.exports = class extends Route {
+    constructor(...args) {
+        super(...args, { route: '/users/find/:username' });
+    }
+
+    get(request, response) {
+        const { username } = request.params;
+        const user = this.client.users.find(user => user.username === username);
+        if (!user) return response.end('{}');
+        return response.end(JSON.stringify(user));
+    }
+}
