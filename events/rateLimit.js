eddd568b7d220fbea80c19947232a10d0b5f0d6a
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-12 19:19:47 -0400

    events

---
+++ events/rateLimit.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
+const { Event, Colors } = require('klasa');
+
+/**
+ * KlasaConsoleConfig#useColor must be
+ * enabled in order to use this piece.
+ */
+
+const HEADER = new Colors({ text: 'red' }).format('[RATELIMIT]');
+
+module.exports = class extends Event {
+
+    run({ timeout, limit, method, route }) {
+        this.client.emit('verbose', [
+            HEADER,
+            `Timeout: ${timeout}ms`,
+            `Limit: ${limit} requests`,
+            `Method: ${method.toUpperCase()}`,
+            `Route: ${route}`
+        ].join('\n'));
+    }
+
+};
