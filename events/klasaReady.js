249441f027e78226cb133e035008afb76a787625
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-12 20:18:38 -0400

    ready event for task

---
+++ events/klasaReady.js
@@ -0,0 +1,11 @@
+const { Event } = require('klasa');
+
+module.exports = class extends Event {
+    constructor(...args) {
+        super(...args, { once: true });
+    }
+
+    async run() {
+        this.client.schedule.create('cleanup', '*/30 * * * *');
+    }
+};
