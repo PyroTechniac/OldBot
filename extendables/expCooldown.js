860649165f1f430d7a56525d58c7fbc7c454f085
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-13 10:54:08 -0400

    extender setters and getters

--- extendables/expCooldown.js
+++ extendables/expCooldown.js
@@ -5,6 +5,16 @@ class UserExtender extends Extendable {
     constructor(...args) {
         super(...args, { appliesTo: [User] });
     }
+
+    get cooldown() {
+        return this.cooling;
+    }
+
+    set cooldown(value) {
+        this.cooling = value;
+    }
 }
+
+UserExtender.prototype.cooling = false;
 
 module.exports = UserExtender;
