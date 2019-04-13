ac4fcf59a2304691e5a04cad7c839dedb04e20b4
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-12 20:10:49 -0400

    casing

--- commands/Tools/remindme.js
+++ commands/Tools/remindme.js
@@ -10,7 +10,7 @@ module.exports = class extends Command {
 
     constructor(...args) {
         super(...args, {
-            description: 'creates a reminder',
+            description: 'Creates a reminder',
             usage: '<when:time> <text:...str>',
             usageDelim: ', '
         });
