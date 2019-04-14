162b978e19f551a883956713c614104b67672289
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-14 13:47:11 -0400

    invite route

---
+++ routes/invite.js
@@ -0,0 +1,11 @@
+const { Route } = require('klasa-dashboard-hooks');
+
+module.exports = class extends Route {
+    constructor(...args) {
+        super(...args, { route: 'invite' });
+    }
+
+    get(request, response) {
+        return response.end(JSON.stringify(this.client.invite));
+    }
+};
