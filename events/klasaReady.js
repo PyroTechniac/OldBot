0011dfa9decf3399704a834ca1748b3362db9843
Owen Bellish <shadowguy224@live.com>
2019-04-16 14:27:26 -0400

    testing something

--- events/klasaReady.js
+++ events/klasaReady.js
@@ -7,7 +7,10 @@ module.exports = class extends Event {
 
     async run() {
         const { tasks } = this.client.schedule;
-        if (!tasks.some(t => t.taskName === 'cleanup')) await this.client.schedule.create('cleanup', '@daily');
+        if (!tasks.some(t => t.taskName === 'cleanup')) {
+            this.client.emit('log', 'Creating task cleanup');
+            await this.client.schedule.create('cleanup', '@daily');
+        }
         await this.client.user.setPresence({
             activity: {
                 type: 'PLAYING',
