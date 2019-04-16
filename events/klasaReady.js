d7029d392f001c0b945186591755ca83b386849c
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-16 15:22:43 -0400

    testing again

--- events/klasaReady.js
+++ events/klasaReady.js
@@ -2,7 +2,7 @@ const { Event } = require('klasa');
 
 module.exports = class extends Event {
     constructor(...args) {
-        super(...args, { once: true, event: 'klasaReady' });
+        super(...args, { once: false, event: 'klasaReady' });
     }
 
     async run() {
